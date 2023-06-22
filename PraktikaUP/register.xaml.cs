dd9863b96df12e57bc067783f3813f1d03bc972e
pr130dulskiy <pr130dulskiy@1-212-CLS-04>
2023-06-22 16:52:50 +0600

    4 день

--- PraktikaUP/register.xaml.cs
+++ PraktikaUP/register.xaml.cs
@@ -4,37 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using 
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PraktikaUP
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для register.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class register : Window
     {
-        public MainWindow()
+        public register()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        private void register_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            new register().Show();
-            this.Close();
         }
     }
 }
